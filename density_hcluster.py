1345d55054497cc17e77899e50dfc8aae309dddf
东凡 <fan.dong@irootech.com>
2018-08-30 19:23:39 +0800

    density is better and scalable

--- density_hcluster.py
+++ density_hcluster.py
@@ -3,8 +3,6 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.signal as signal
-from sklearn.cluster import KMeans
-from sklearn import mixture
 import statsmodels.nonparametric.api as smnp
 
 
@@ -46,6 +44,48 @@ def fillin_missvalue(data, w):
     return data
 
 
+def better_local(peaks, vallys, x_plot):
+    if len(peaks) <= 1:
+        print('One peak')
+        return pd.DataFrame(columns=['x_plot', 'x_density']), np.nan
+    new_vallys = []
+    for i, _ in enumerate(peaks[:-1]):
+        temp = vallys[(vallys > peaks[i]) & (vallys < peaks[i + 1])]
+        new_vallys.append(int(np.min(temp)))
+
+        x_plot = x_plot.reshape(-1)
+        local_minmax = pd.DataFrame({'x': np.concatenate((x_plot[peaks], x_plot[new_vallys])),
+                                     'y': np.concatenate((x_dens[peaks], x_dens[new_vallys]))})
+        local_minmax.sort_values(by='x', inplace=True)
+        local_minmax.reset_index(drop=True, inplace=True)
+
+        return local_minmax
+
+
+def get_sum_label(data_raw_2, local_min, m):
+    final_data = data_raw_2.copy()
+    threshold = local_min[m]
+    sum_label = np.zeros(final_data.shape[0])
+
+    sum_label[(final_data['Ia'] < 0.1)] = 0
+    sum_label[(final_data['Ia'] >= 0.1) & (final_data['Ia'] < threshold)] = 1
+    sum_label[final_data['Ia'] >= threshold] = 2
+    final_data['sum_label'] = sum_label
+
+    return final_data
+
+
+def plot_final(data):
+    """
+    画出根据m动态调整后的图
+    """
+    colors = ['grey', 'green', 'red']
+    for i in range(3):
+        plt.scatter(data.index[data['sum_label'] == i].values,
+                    data.loc[data['sum_label'] == i, 'Ia'].values,
+                    s=2, color=colors[i])
+
+
 ##
 if __name__ == '__main__':
     ip_list = ['10.9.129.31', '10.9.129.30',
@@ -55,7 +95,7 @@ if __name__ == '__main__':
                '10.9.129.79', '10.9.130.75', '10.9.129.96']
 
     # 数据预处理
-    df_raw = pd.read_csv(r'./%s.csv' % ip_list[-1])
+    df_raw = pd.read_csv(r'./%s.csv' % ip_list[0])
     data_raw_1 = load_data(df_raw)
     data_raw_2 = fillin_missvalue(data_raw_1, 10)
     data_raw_3 = fillin_missvalue(data_raw_2, 10)
@@ -65,4 +105,23 @@ if __name__ == '__main__':
     kde = smnp.KDEUnivariate(x_density)
     kde.fit(kernel='gau', bw='scott', fft=True, gridsize=100, cut=3.0, clip=(-np.inf, np.inf))
     x_plot, x_dens = kde.support, kde.density
+
+    peaks, p_heights = signal.find_peaks(x_dens, height=0.03 * max(x_dens))
+    vallys, v_heights = signal.find_peaks(-x_dens)
+
     plt.plot(x_plot, x_dens)
+    plt.scatter(x_plot[peaks], x_dens[peaks], color='red')
+    plt.scatter(x_plot[vallys], x_dens[vallys], color='green')
+
+    local_minmax = better_local(peaks, vallys, x_plot)
+
+    plt.plot(x_plot, x_dens)
+    plt.scatter(local_minmax.x, local_minmax.y)
+
+    odd = list(map(lambda x: x % 2 == 1, local_minmax.index))
+    local_min = local_minmax.x[odd].values
+
+    final_data = get_sum_label(data_raw_2, local_min, m=1)
+    plot_final(final_data)
+
+
